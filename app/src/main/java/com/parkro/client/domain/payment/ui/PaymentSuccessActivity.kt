367425e970a434090c43504564a85fe63125d74b
김지수 <wltn2858@swu.ac.kr>
2024-07-31 18:01:29 +0900

    :sparkles: Feat : 사전 정산 페이지

--- app/src/main/java/com/parkro/client/domain/payment/ui/PaymentSuccessActivity.kt
+++ app/src/main/java/com/parkro/client/domain/payment/ui/PaymentSuccessActivity.kt
@@ -1,21 +1,34 @@
 package com.parkro.client.domain.payment.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.parkro.client.MainActivity
 import com.parkro.client.R
 
-// 결제가 성공적으로 완료됐을 때 돌아오는 Activity
 class PaymentSuccessActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment_success)
 
+        // 필요한 데이터 가져오기
         val orderId = intent.getStringExtra("orderId")
         val amount = intent.getStringExtra("amount")
 
-        val textView = findViewById<TextView>(R.id.successTextView)
-        textView.text = "Order ID: $orderId\nAmount: $amount"
+        // 결제 성공 메시지 설정
+        val successTextView: TextView = findViewById(R.id.successTextView)
+        successTextView.text = "결제 완료!\nOrder ID: $orderId \nAmount: $amount"
+
+        // 결제 완료 버튼 클릭 리스너 설정
+        val btnCompletePayment: Button = findViewById(R.id.btnCompletePayment)
+        btnCompletePayment.setOnClickListener {
+            // MainActivity로 이동
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/parkro/client/domain/payment/ui/PaymentWebViewActivity.kt
+++ app/src/main/java/com/parkro/client/domain/payment/ui/PaymentWebViewActivity.kt
@@ -4,9 +4,9 @@ import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.*
+import androidx.appcompat.app.AppCompatActivity
 import com.parkro.client.BuildConfig
 import com.parkro.client.R
 import java.net.URISyntaxException
@@ -14,10 +14,18 @@ import java.net.URISyntaxException
 // 결제창 웹뷰로 띄울 Activity
 class PaymentWebViewActivity : AppCompatActivity() {
     private lateinit var webView: WebView
+    private val paymentData = mutableMapOf<String, String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment_web_view)
+
+        // Intent로부터 데이터 가져오기
+        val intent = intent
+        paymentData["amount"] = intent.getStringExtra("amount") ?: "0"
+        paymentData["orderId"] = intent.getStringExtra("orderId") ?: "ORDER_ID"
+        paymentData["orderName"] = intent.getStringExtra("orderName") ?: "주차장 사전 결제"
+        paymentData["customerName"] = intent.getStringExtra("customerName") ?: ""
 
         webView = findViewById(R.id.webview)
 
@@ -25,6 +33,7 @@ class PaymentWebViewActivity : AppCompatActivity() {
             javaScriptEnabled = true
             javaScriptCanOpenWindowsAutomatically = true
             cacheMode = WebSettings.LOAD_NO_CACHE
+            defaultTextEncodingName = "utf-8"
             setSupportMultipleWindows(true)
         }
 
@@ -44,17 +53,16 @@ class PaymentWebViewActivity : AppCompatActivity() {
                 } else super.shouldOverrideUrlLoading(view, url)
             }
 
+            // WebViewActivity에서 결제 성공 후
             private fun handleUrl(url: String): Boolean {
                 return if (!URLUtil.isNetworkUrl(url) && !URLUtil.isJavaScriptUrl(url)) {
                     val uri = Uri.parse(url)
                     if ("parkro" == uri.scheme) {
-                        val orderId = uri.getQueryParameter("orderId")
-                        val amount = uri.getQueryParameter("amount")
-                        val intent = Intent(this@PaymentWebViewActivity, PaymentSuccessActivity::class.java).apply {
-                            putExtra("orderId", orderId)
-                            putExtra("amount", amount)
-                        }
+                        val intent = Intent(this@PaymentWebViewActivity, PaymentSuccessActivity::class.java)
+                        intent.putExtra("orderId", paymentData["orderId"])
+                        intent.putExtra("amount", paymentData["amount"])
                         startActivity(intent)
+                        finish() // 현재 Activity 종료
                         true
                     } else {
                         handleScheme(url)
@@ -82,7 +90,10 @@ class PaymentWebViewActivity : AppCompatActivity() {
             }
         }
 
-        // 결제 페이지 로드
+        val webAppInterface = WebAppInterface()
+        webView.addJavascriptInterface(webAppInterface, "Android")
+
+        // 웹뷰 로드
         webView.loadUrl("file:///android_asset/payment.html")
     }
 
@@ -95,6 +106,12 @@ class PaymentWebViewActivity : AppCompatActivity() {
         @JavascriptInterface
         fun getServerBaseUrl(): String {
             return BuildConfig.SERVER_BASEURL
+        }
+
+        @JavascriptInterface
+        fun getPaymentData(key: String): String {
+            println("getpaymentdata: " + key + " " + paymentData[key])
+            return paymentData[key] ?: ""
         }
     }
 }
